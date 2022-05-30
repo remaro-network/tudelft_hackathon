f27e8c9af300b0f743112df9d1e9bb01ffc0ee1c
Gustavo <g.rezendesilva@tudelft.nl>
2022-05-30 13:15:26 +0200

    🔧 launchfile for simulation

---
+++ launch/bluerov_ign_sim.launch.py
@@ -0,0 +1,38 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
+
+    ign_gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_ign_gazebo, 'launch', 'ign_gazebo.launch.py')),
+        launch_arguments={
+           'ign_args': '-r bluerov_pipeline.world'
+        }.items(),
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_ign_bridge',
+        executable='parameter_bridge',
+        arguments=['lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        ign_gazebo,
+        bridge,
+    ])
