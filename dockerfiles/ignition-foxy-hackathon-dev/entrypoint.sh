478a0892456bd48b4b0c3f1fbf9833e6d275c79f
Gustavo <g.rezendesilva@tudelft.nl>
2022-06-13 18:21:42 +0200

    🔧 build ingition-foxy-hackathon-dev from ignition ardusub img

--- dockerfiles/ignition-foxy-hackathon-dev/entrypoint.sh
+++ dockerfiles/ignition-foxy-hackathon-dev/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 source "/opt/ros/foxy/setup.bash"
 source "/ign_ws/install/setup.bash"
-source "/tudelft_hackathon_ws/install/setup.bash"
+source "/home/docker/tudelft_hackathon_ws/install/setup.bash"
+source "/home/docker/ardupilot/Tools/completion/completion.bash"
+. ~/.profile
 
 exec "$@"
